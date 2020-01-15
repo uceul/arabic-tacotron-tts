0c85be95e626664b2da1db19cf400b66ef49f3f7
uceul <uceul@student.kit.edu>
2020-01-15 11:36:10 +0100

    Added work loop stub and some helper methods

--- test.py
+++ test.py
@@ -3,14 +3,36 @@
 import argparse
 from hparams import hparams, hparams_debug_string
 import os
+import time
 from synthesizer import Synthesizer
 
-PATH_TO_MODEL="../model/model.ckpt-200000"
+PATH_TO_MODEL = "../model/model.ckpt-200000"
+TEXT_DIR = "test/text"
+SPEECH_DIR = "test/speech"
 
 synthesizer = Synthesizer()
 
+def work_loop():
+  while(True):
+    if not (os.listdir(TEXT_DIR)):
+      print("Text directory empty!")
+      time.sleep(5)
+    else:
+      print("Text directory is not empty!")
+      time.sleep(5)
+
+def save_wav_file(audio_data):
+  filename = "speech" + int(time.time()) + ".wav"
+  wav_file_path = os.path.join(SPEECH_DIR, filename)
+  wav_file = open(wav_file_path, "wb")
+  wav_file.write(audio_data)
+  wav_file.close()
+  print(f"Wrote to {wav_file_path}")
+
+def synthesize_from_file(file):
+  pass
+
 if __name__ == '__main__':
-  from wsgiref import simple_server
   parser = argparse.ArgumentParser()
   parser.add_argument('--checkpoint', default=PATH_TO_MODEL)
   parser.add_argument('--port', type=int, default=9200)
@@ -21,13 +43,10 @@ if __name__ == '__main__':
   hparams.parse(args.hparams)
   print(hparams_debug_string())
   synthesizer.load(args.checkpoint)
-  print("================LOADED======================")
+  print("================MODEL LOADED======================")
   synthesis = synthesizer.synthesize(" لَا تَثِقْ فِي كُلِّ مَا تَرَاهُ ")
-  filename = "speech.wav"
-  wav_file = open(filename, 'wb')
-  wav_file.write(synthesis)
-  wav_file.close()
-  print("=========WROTE TO FILE============")
+  save_wav_file(synthesis)
+  work_loop()
 else:
   synthesizer.load(os.environ['CHECKPOINT'])
 
